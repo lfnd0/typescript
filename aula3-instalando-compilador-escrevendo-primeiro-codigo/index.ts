0df3b25e19bb9ced25336fc0c5ee9c0a0d0b7f75
luizfernando <luiz-fsilva37@hotmail.com>
2021-12-31 17:37:09 -0300

    Atualização aula 3

--- aula3-instalando-compilador-escrevendo-primeiro-codigo/index.ts
+++ aula3-instalando-compilador-escrevendo-primeiro-codigo/index.ts
@@ -3,9 +3,9 @@ const input2 = document.getElementById('num2') as HTMLInputElement;
 const button = document.getElementById('button');
 
 function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 button.addEventListener('click', function () {
-    console.log(sum(Number(input1.value), Number(input2.value)));
+  console.log(sum(Number(input1.value), Number(input2.value)));
 });
