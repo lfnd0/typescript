71032fb1cfb64720937a4021ad030d4c980f18fe
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-01 14:49:30 -0300

    Adição aula 6

---
+++ aula6-type-inference/typeInference.ts
@@ -0,0 +1,7 @@
+// type inference (inferência de tipagem "automática")
+let message = 'Mensagem 1';
+message = 'Mensagem 2';
+
+window.addEventListener('click', (e) => {
+  console.log(e.target);
+});
